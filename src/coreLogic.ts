3cda3e160d1e6b254d52b56f790d73a0656a851e
soma <soma@koii.network>
2023-03-22 11:29:36 -0300

    add more API TODO

--- src/coreLogic.ts
+++ src/coreLogic.ts
@@ -10,22 +10,31 @@ class CoreLogic {
 
       if (namespace.app) { // Express app for configuration
 
-        namespace.express('get', 'test', async (req, res) => {
+        // Testing API
+        namespace.express('get', 'test', async (res:any) => { 
           res.send("API TESTING PAGE, GO TO /get-linktree")
         })
         
-        namespace.express('get', '/get-linktree', async (req, res) => {  //API: get all Linktrees data
+        // * GET API: get all Linktrees data
+        namespace.express('get', '/get-linktree', async (res: any) => {  
           try {
-            await namespaceWrapper.storeSet("linktree", "testing") // Set value to db
-            const Linktrees = await namespaceWrapper.storeGet("linktree"); // Get value from db
+            await namespaceWrapper.storeSet("linktree", "testing") // * Set value to db 
+            const Linktrees = await namespaceWrapper.storeGet("linktree"); // * Get value from db
             if (Linktrees) {
               console.log('linktrees =' + Linktrees);
-              res.status(200).send(Linktrees); // print db
+              res.status(200).send(Linktrees); // * print db
             }
           } catch (err) {
             console.log('Catch in Linktrees', err);
           }
         });
+
+        // TODO: POST API: generate a new linktree
+        namespace.express('post', '/generate',async (req: object, res: any) => {
+          
+        })
+
+        // TODO: POST API: update a exist linktree
 
       }
     } catch (err) {
