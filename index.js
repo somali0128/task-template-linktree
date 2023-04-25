42ff8b016f9e18e06e5503de3b39c9dfe71a1256
soma <soma@koii.network>
2023-04-25 12:47:27 -0300

    update feaeture, clean logs every 3 day

--- index.js
+++ index.js
@@ -13,19 +13,49 @@ const db = require('./db_model');
 const routes = require('./routes');
 
 async function setup() {
+  
   const originalConsoleLog = console.log;
-
+  const logDir = './namespace';
+  const logFile = 'logs.txt';
+  const maxLogAgeInDays = 3;
+  
+  // Check if the log directory exists, if not, create it
+  if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+  }
+  
   // Create a writable stream to the log file
-  const logStream = fs.createWriteStream('./namespace/logs.txt', { flags: 'a' });
-
+  const logPath = path.join(logDir, logFile);
+  const logStream = fs.createWriteStream(logPath, { flags: 'a' });
+  
+  // Function to remove logs older than specified age (in 3 days)
+  async function cleanOldLogs(logDir, logFile, maxLogAgeInDays) {
+    const currentDate = new Date();
+    const logPath = path.join(logDir, logFile);
+  
+    if (fs.existsSync(logPath)) {
+      const fileStats = fs.statSync(logPath);
+      const fileAgeInDays = (currentDate - fileStats.mtime) / (1000 * 60 * 60 * 24);
+  
+      if (fileAgeInDays > maxLogAgeInDays) {
+        fs.unlinkSync(logPath);
+      }
+    }
+  }
+  
   // Overwrite the console.log function to write to the log file
   console.log = function (...args) {
     originalConsoleLog.apply(console, args);
-    const message = args.map(arg => (typeof arg === 'object' ? JSON.stringify(arg) : arg)).join(' ') + '\n';
-
+    const message = args
+      .map(arg => (typeof arg === 'object' ? JSON.stringify(arg) : arg))
+      .join(' ') + '\n';
+  
     // Write the message to the log file
     logStream.write(message);
   };
+  
+  // Clean old logs
+  await cleanOldLogs(logDir, logFile, maxLogAgeInDays);
 
   console.log("setup function called");
   // Run default setup
